2cf83e013e6b5c4ffe3f0c5742a7e32733f289ac
unix_system <4990534+unix-system@users.noreply.github.com>
2023-08-08 10:53:13 +0100

    Deployment config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,11 +15,12 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/jsdotlua.github.io",
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "unix-system", // Usually your GitHub org/user name.
-  projectName: "unix-system.github.io/jsdotlua.github.io", // Usually your repo name.
+  projectName: "unix-system/jsdotlua.github.io", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
